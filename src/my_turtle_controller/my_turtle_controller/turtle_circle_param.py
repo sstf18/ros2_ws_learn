5fc049ea426dc2985aefed6a4c5c01d65bd2c6fb
sstf18 <sstf18@gmail.com>
2025-11-14 00:28:08 +0000

    Update the turtle_circle_param.py for better understanding

--- src/my_turtle_controller/my_turtle_controller/turtle_circle_param.py
+++ src/my_turtle_controller/my_turtle_controller/turtle_circle_param.py
@@ -9,16 +9,17 @@ class TurtleCircleParam(Node):
     def __init__(self):
         super().__init__('turtle_circle_param')
 
-        # 声明参数（带默认值）
-        self.declare_parameter('linear_speed', 2.0)    # 线速度
-        self.declare_parameter('angular_speed', 1.0)   # 角速度
-        self.declare_parameter('duration', 2 * math.pi)  # 运动时间（秒），默认大约转一圈
+        #declare a specific parameter for linear, angular
+        self.declare_parameter('linear_speed', 2.0)    # linear speed
+        self.declare_parameter('angular_speed', 1.0)   # angualr speed
+        self.declare_parameter('duration', 2 * math.pi)  # duration
 
-        # 读取参数
+        # get_parameter().vaue: Retrieves the current value for the declared parameter and stores it in a python class attribute
         self.linear_speed = self.get_parameter('linear_speed').value
         self.angular_speed = self.get_parameter('angular_speed').value
         self.duration = self.get_parameter('duration').value
 
+        #ROS2 standard way to print an informational message to the console
         self.get_logger().info(
             f'linear_speed={self.linear_speed}, angular_speed={self.angular_speed}, '
             f'duration={self.duration}'
@@ -26,29 +27,30 @@ class TurtleCircleParam(Node):
 
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
 
-        # 要发布的速度指令
+        # Creates the Twist message object
         self.cmd = Twist()
+        
+        # Sets the message's forward speed using the value read from the parameter
         self.cmd.linear.x = self.linear_speed
+        
+        
         self.cmd.angular.z = self.angular_speed
 
-        # 记录开始时间
+        # Records the exact time the node started running
         self.start_time = time.time()
 
-        # 50ms 调用一次回调，大约 20Hz
+        # 
         self.timer = self.create_timer(0.05, self.timer_callback)
 
     def timer_callback(self):
         elapsed = time.time() - self.start_time
 
         if elapsed < self.duration:
-            # 还没到指定时间，继续转圈
             self.publisher_.publish(self.cmd)
         else:
-            # 到时间了，发送停止指令，然后关闭节点
             stop_cmd = Twist()
             self.publisher_.publish(stop_cmd)
             self.get_logger().info('Finished circle, stopping turtle.')
-            # 这里直接关掉 rclpy，结束程序
             rclpy.shutdown()
 
 
