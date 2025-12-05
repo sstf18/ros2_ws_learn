45dc2e814acdf3a0738ba5a635fee4a8d4e31449
sstf18 <sstf18@gmail.com>
2025-12-05 15:48:34 -0800

    update launch file

--- src/my_turtle_controller/launch/turtle_follow_demo.launch.py
+++ src/my_turtle_controller/launch/turtle_follow_demo.launch.py
@@ -1,16 +1,32 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess 
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
 
 
 def generate_launch_description():
+    linear_gain = LaunchConfiguration('linear_gain')
+    angular_gain = LaunchConfiguration('angular_gain')
+    
     return LaunchDescription([
+        # ===== 1. Launch parameter declare =====
+        DeclareLaunchArgument(
+            'linear_gain',
+            default_value='1.0',
+            description='P controller linear gain for follower'
+        ),
+        DeclareLaunchArgument(
+            'angular_gain',
+            default_value='6.0',
+            description='P controller angular gain for follower'
+        ),
 
-        # 1. Start turtlesim
+        # 2. Start turtlesim
         Node(
             package='turtlesim',
             executable='turtlesim_node',
             name='sim',
+            output='screen'
         ),
 
         # 3. Spawn turtle2
@@ -20,7 +36,7 @@ def generate_launch_description():
                 'turtlesim/srv/Spawn',
                 "{x: 2.0, y: 8.0, theta: 0.0, name: 'turtle2'}",
             ],
-            output='screen',
+            output='screen'
         ),
 
         # 4. turtle1 broadcaster
@@ -28,6 +44,7 @@ def generate_launch_description():
             package='my_turtle_controller',
             executable='turtle_tf_broadcaster',
             name='turtle1_broadcaster',
+            output='screen'
         ),
 
         # 5. turtle2 broadcaster
@@ -35,6 +52,7 @@ def generate_launch_description():
             package='my_turtle_controller',
             executable='turtle2_tf_broadcaster',
             name='turtle2_broadcaster',
+            output='screen'
         ),
 
         # 6. let turtle2 follow turtle1
@@ -42,7 +60,12 @@ def generate_launch_description():
             package='my_turtle_controller',
             executable='turtle2_tf_follow_turtle1',
             name='turtle2_follower',
-        ),
+            parameters=[
+                {'linear_gain': linear_gain},
+                {'angular_gain': angular_gain},            
+            ],
+            output='screen'
+        ),   
     ])
 
 
--- src/my_turtle_controller/my_turtle_controller/turtle2_tf_follow_turtle1.py
+++ src/my_turtle_controller/my_turtle_controller/turtle2_tf_follow_turtle1.py
@@ -2,30 +2,40 @@ import math
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from tf2_ros import Buffer, TransformListener, LookupException
+from tf2_ros import Buffer, TransformListener, TransformException
 
 
 class Turtle2FollowTurtle1(Node):
     def __init__(self):
         super().__init__('turtle2_tf_follow_turtle1')
 
-        self.pub = self.create_publisher(Twist, '/turtle2/cmd_vel', 10)
+        # declare parameter 
+        self.declare_parameter('linear_gain', 1.0)
+        self.declare_parameter('angular_gain', 6.0)
+	
+        self.k_lin =    self.get_parameter('linear_gain').get_parameter_value().double_value
+        self.k_ang = self.get_parameter('angular_gain').get_parameter_value().double_value
+	
+        # pubulish to turtle2 cmd_vel
+        self.cmd_pub = self.create_publisher(Twist, '/turtle2/cmd_vel', 10)
 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        self.k_lin = 1.0
-        self.k_ang = 6.0
-
         self.timer = self.create_timer(0.05, self.follow)
+        
+        self.get_logger().info(
+            f"Turtle2 following turtle1 with gains: "
+            f"linear_gain={self.k_lin}, angular_gain={self.k_ang}"
+        )
 
     def follow(self):
         try:
             # lookup turtle1 relative to turtle2
             tf = self.tf_buffer.lookup_transform(
                 'turtle2',     # target frame
-		'turtle1',     # source frame
-		rclpy.time.Time()
+	        'turtle1',     # source frame
+	        rclpy.time.Time()
             )
 
             dx = tf.transform.translation.x
@@ -38,14 +48,14 @@ class Turtle2FollowTurtle1(Node):
 
             # stop when very close
             if dist < 0.3:
-                self.pub.publish(cmd)
+                self.cmd_pub.publish(cmd)
                 return
 
             # simple P control
             cmd.angular.z = self.k_ang * angle
             cmd.linear.x = self.k_lin * dist * max(0.0, math.cos(angle))
 
-            self.pub.publish(cmd)
+            self.cmd_pub.publish(cmd)
 
         except LookupException:
             # TF not ready yet
